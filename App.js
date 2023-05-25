daaa5fad87306134bf7c5b0128ecbd9e6f707435
HyperClap <MrGromFullYou@yandex.ru>
2023-05-25 11:22:35 +0300

    Добавление навигации по профилю, настройка GestureHandler

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
 import {Platform, SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import HomePage from "./pages/HomePage";
-import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage"
 import {NavigationContainer} from "@react-navigation/native";
 import CatalogNavigation from "./nav/CatalogNavigation";
 import ToViewNavigation from "./nav/ToViewNavigation";
+import {ProfileNavigation} from "./nav/ProfileNavigation";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 export default function App() {
   const Tab = createBottomTabNavigator();
   return (
-      <SafeAreaView style={AppStyles.container}>
-          <StatusBar barStyle={"dark-content"}/>
-          <NavigationContainer>
-              <Tab.Navigator>
-                  <Tab.Screen options={{ headerShown: false }} name="Главная" component={HomePage} />
-                  <Tab.Screen options={{ headerShown: false }} name="К просмотру" component={ToViewNavigation} />
-                  <Tab.Screen options={{ headerShown: false }} name="Каталог" component={CatalogNavigation} />
-                  <Tab.Screen options={{ headerShown: false }} name="Профиль" component={ProfilePage} />
-              </Tab.Navigator>
-          </NavigationContainer>
-      </SafeAreaView>
+          <GestureHandlerRootView style={{flex: 1}}>
+              <SafeAreaView style={AppStyles.container}>
+                  <StatusBar barStyle={"dark-content"}/>
+                  <NavigationContainer >
+                      <Tab.Navigator>
+                          <Tab.Screen options={{ headerShown: false }} name="Главная" component={HomePage} />
+                          <Tab.Screen options={{ headerShown: false }} name="К просмотру" component={ToViewNavigation} />
+                          <Tab.Screen options={{ headerShown: false }} name="Каталог" component={CatalogNavigation} />
+                          <Tab.Screen options={{ headerShown: false }} name="Профиль" component={ProfileNavigation} />
+                      </Tab.Navigator>
+                  </NavigationContainer>
+              </SafeAreaView>
+          </GestureHandlerRootView>
   );
 }
 
