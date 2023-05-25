04c2090af267c0bc7fe85c02edbc71ad867788bd
HyperClap <MrGromFullYou@yandex.ru>
2023-05-25 11:16:33 +0300

    Компонент профиля

---
+++ components/Profile/Profile.js
@@ -0,0 +1,24 @@
+import {StyleSheet, View} from "react-native";
+import {userStore} from "../../store";
+import {observer} from "mobx-react-lite";
+import AuthorizedComponent from "./AuthorizedComponent";
+import NotAuthorizedComponent from "./NotAuthorizedComponent";
+
+const Profile = observer(() => {
+    return (
+        <View style={styles.wrapper}>
+            {userStore.isLogined ? <AuthorizedComponent/> : <NotAuthorizedComponent/>}
+        </View>
+    );
+});
+
+const styles = StyleSheet.create({
+    wrapper: {
+        paddingTop: 20,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
+
+export default Profile;
