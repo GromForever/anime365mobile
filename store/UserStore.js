3f892fa4477882738351d40666f0206c1939fbb4
HyperClap <MrGromFullYou@yandex.ru>
2023-05-25 11:14:24 +0300

    Исправлена логика mobx store UserStore, теперь работает правильно

--- store/UserStore.js
+++ store/UserStore.js
@@ -1,41 +1,65 @@
-import {makeAutoObservable} from "mobx";
+import {action, makeObservable, observable, runInAction} from "mobx";
 import {GetInfoAboutUser, LoginRequest} from "../api/user";
 import AsyncStorageNative from "@react-native-async-storage/async-storage/src/AsyncStorage.native";
 import UserHelper from "../common/userHelper";
+import {Platform} from "react-native";
 
 
 export default class UserStore {
-    userData
-    token
+    userData = {}
+    token = null;
+    isLogined = false;
 
     constructor() {
-        makeAutoObservable(this);
+        makeObservable(this, {
+            userData: observable,
+            token: observable,
+            isLogined: observable,
+            restoreSession: action,
+            login: action,
+            logout: action
+        })
         this.restoreSession();
     }
 
     async restoreSession() {
-        const token = await AsyncStorageNative.getItem("access_token");
-        if (token === null) {
-            this.userData = {}
-            this.token = null;
-            return;
+        if (Platform.OS !== "web") {
+            const token = await AsyncStorageNative.getItem("access_token");
+            if (token === null) {
+                runInAction(() => {
+                    this.userData = {}
+                    this.token = null;
+                    this.isLogined = false;
+                })
+                return;
+            }
+            const data = await GetInfoAboutUser(token);
+            if (UserHelper.isGuest(data)) {
+                runInAction(() => {
+                    this.token = null;
+                    this.userData = {};
+                    this.isLogined = false
+                })
+                await AsyncStorageNative.removeItem("access_token");
+                return;
+            }
+            runInAction(() => {
+                this.token = token;
+                this.userData = data;
+                this.isLogined = true
+            })
         }
-        this.token = token;
-        const data = await GetInfoAboutUser(token);
-        if (UserHelper.isGuest(data)) {
-            this.token = null;
-            this.userData = {};
-            await AsyncStorageNative.removeItem("access_token");
-            return;
-        }
-        this.userData = data;
     }
 
     async login(login, password) {
         const tokenOrError = await LoginRequest(login, password);
         if (tokenOrError.message === undefined) {
-            this.token = tokenOrError;
-            this.userData = await GetInfoAboutUser(tokenOrError)
+            const infoAboutUser = await GetInfoAboutUser(tokenOrError)
+            runInAction(() => {
+                this.token = tokenOrError;
+                this.userData = infoAboutUser;
+                this.isLogined = true;
+            })
             await AsyncStorageNative.setItem("access_token", tokenOrError)
             return true;
         }
@@ -43,8 +67,11 @@ export default class UserStore {
     }
 
     async logout() {
+        runInAction(() => {
+            this.userData = {}
+            this.token = null;
+            this.isLogined = false;
+        })
         await AsyncStorageNative.removeItem("access_token");
-        this.userData = {}
-        this.token = null;
     }
 }
