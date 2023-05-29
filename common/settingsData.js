e27f830ece9d20e98c6b4be828200f0f54a98648
HyperClap <MrGromFullYou@yandex.ru>
2023-05-29 13:51:47 +0300

    Меню настроек "Уведомления"

--- common/settingsData.js
+++ common/settingsData.js
@@ -1,9 +1,14 @@
-import {AntDesign} from "@expo/vector-icons";
+import {AntDesign, Ionicons} from "@expo/vector-icons";
 
 export const data = [
     {
         title: "Внешний вид",
         route: "designSettings",
         icon: <AntDesign name="setting" size={24} color="aqua" />
+    },
+    {
+        title: "Уведомления",
+        route: "notificationsSettings",
+        icon: <Ionicons name="notifications" size={24} color="yellow" />
     }
 ]
